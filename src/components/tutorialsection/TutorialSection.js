c4c1953980d001a35f15246ff6f8a3b4c69524a1
inwonakng <inwon.kang04@gmail.com>
2022-02-14 18:56:56 -0500

    youtube link addded

--- src/components/tutorialsection/TutorialSection.js
+++ src/components/tutorialsection/TutorialSection.js
@@ -16,6 +16,8 @@ const TutorialSection = () => {
                     <h3><b>Introduction</b></h3>
 Voting is the most popular method of making a collective decision from individual (and often contradictory) preference of agents. Many different voting rules are developed for this purpose. Various axioms have been defined in classic social choice literature to measure goodness of a voting rule, such as Condorcet efficiency, monotonicity, anonymity etc [1]. On the other hand, in modern times new properties are also considered important for the decision-making domain such as group fairness, privacy etc. It has been shown that many of these properties are inherently incompatible with each other (e.g.,[2, 3, 4]), thus leading to a trade-off. We propose a machine learning-based method for designing new voting rules at different levels of trade-offs between these properties.<br/>
 The notion of group fairness we use is group-imbalance based fairness [4] and calculates average-case fairness of a voting rule for two groups of different sizes. The notion of privacy we use is local differential privacy [5] and we show the performance of voting rules at different levels of privacy requirement.<br/>
+                    
+                    <a className='vidlink' target='_blank' href='https://www.youtube.com/watch?v=7wMiABG3b84'>Link to video</a>
                     <div className="references">
                         <b>References</b>
                         <br/>
